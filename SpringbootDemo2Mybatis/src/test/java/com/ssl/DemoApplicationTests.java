9a2ac908ec04f1a832ff497b3390b24f02222dd6
coolsslong <1452844985@qq.com>
2019-11-05 19:55:03 +0800

    更新mybatis配置文件

--- SpringbootDemo2Mybatis/src/test/java/com/ssl/DemoApplicationTests.java
+++ SpringbootDemo2Mybatis/src/test/java/com/ssl/DemoApplicationTests.java
@@ -1,5 +1,8 @@
 package com.ssl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.ssl.mapper.UserMapper;
 import com.ssl.model.User;
 import org.junit.Test;
@@ -7,10 +10,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -25,17 +24,33 @@ public class DemoApplicationTests {
     @Test
     public void test() {
         //增加
-        User user = new User("ruiwen", "123456", "女", "xxx");
-        //userMapper.insert(user);
+        for (int i = 0; i < 100; i++) {
+            User user = new User("ruiwen"+i, "123456", "女", "xxx");
+            userMapper.insert(user);
+        }
         //查询
-        System.out.println(userMapper.getAll());
-        //测试缓存，一级缓存默认开启，同一个查询拿到的对象是一样的
-        System.out.println(userMapper.getOne(1L)==userMapper.getOne(1L));
-        user.setNickName("mmmmm");
-        user.setId(2L);
-        userMapper.update(user);
-        userMapper.delete(1L);
+//        System.out.println(userMapper.getAll());
+//        //测试缓存，一级缓存默认开启，同一个查询拿到的对象是一样的
+//        System.out.println(userMapper.getOne(1L)==userMapper.getOne(1L));
+//        user.setNickName("mmmmm");
+//        user.setId(2L);
+//        userMapper.update(user);
+//        userMapper.delete(1L);
     }
 
+    @Test
+    public void test1() {
+        Page<Object> page = PageHelper.startPage(2, 10);
+        userMapper.getAll().forEach(user -> System.out.println(user));
+        PageInfo<Object> pageInfo = new PageInfo<>();
+        System.out.println("page.getPageNum() = " + page.getPageNum());
+        System.out.println("page.getTotal() = " + page.getTotal());
+        System.out.println("page.getPageSize() = " + page.getPageSize());
+        System.out.println("page.getPages() = " + page.getPages());
+    }
 
+    @Test
+    public void test2() {
+
+    }
 }
